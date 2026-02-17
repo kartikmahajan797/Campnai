f96f00363fec08588999a7a2dd9b78a25abab67b
Arnav <arnavprajapati3101@gmail.com>
2026-02-17 09:56:49 +0530

    .

--- server/src/index.js
+++ server/src/index.js
@@ -22,10 +22,7 @@ app.use(hpp());
 const allowedOrigins = [
     "http://localhost:3000",
     "http://localhost:5173",
-    "http://localhost:8080",
-    "http://127.0.0.1:3000",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:8080",
+    "http://localhost:8081",
     process.env.FRONTEND_URL,
 ].filter(Boolean);
 
